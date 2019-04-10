a890ec1e6e75e5f63ab81f06c3f59e43652366e8
glz-diego <glzdiego12@gmail.com>
2019-04-09 22:04:58 -0500

    init message

--- script.js
+++ script.js
@@ -1,37 +1,102 @@
-const spaces = [];
-const carsTotal = [];
+var spaces = 10;
+var carsTotal = 100;
+var carsGone = 0;
 
-function car(){
-	function makeName(){
-		var make = '';
-		var brand = ["Ford", "Mustang", "Nissan", "Jeep", "Toyota", "Honda", "Porsche", "Volvo", "Subaru", "Chevrolet"];
-		make = brand[Math.floor(Math.random() * 10)];
-		console.log(make);
-	}
-
-	function plateID(){
-		var plate = "";
-		var id = 'qwertyuiopasdfghjklzxcvbnm0123456789';
-		for (var i = 0; i <= 6; i++) {
-			plate += id.charAt(Math.floor(Math.random() * 36))
-		}
-		console.log(plate);
-	}
-
-	function hexID(){
-		var hex = "";
-		var colorID = '0123456789';
-		for (var i = 0; i <= 5; i++) {
-			hex += colorID.charAt(Math.floor(Math.random() * 10))
-		}
-		console.log(hex);
-	}
-
-	function setUp(){
-		makeName();
-		plateID();
-		hexID();
-	}
-	setUp();
+	var make = "";
+function makeName(){
+	var brand = ["Ford", "Mustang", "Nissan", "Jeep", "Toyota", "Honda", "Porsche", "Volvo", "Subaru", "Chevrolet"];
+	make = brand[Math.floor(Math.random() * 10)];
 }
-car();
+
+	var plate = "";
+function plateID(){
+	plate = "";
+	var id = 'qwertyuiopasdfghjklzxcvbnm0123456789';
+	for (var i = 0; i <= 6; i++) {
+		plate += id.charAt(Math.floor(Math.random() * 36))
+	}
+}
+
+	var hex = "";
+function hexID(){
+	hex = "";
+	var colorID = '0123456789';
+	for (var i = 0; i <= 5; i++) {
+		hex += colorID.charAt(Math.floor(Math.random() * 10))
+	}
+}
+
+	var timerTime = "";
+// function timer() {
+// 	timerTime = "";
+// 	var possibleSec = '0123456789';
+// 	for (var i = 0; i <= 1; i++) {
+// 		timerTime += possibleSec.charAt(Math.floor(Math.random() * 5))
+// 	}
+// }
+
+function clearInterval() {
+	clearInterval(timerId);
+}
+
+function park(){
+	var carsWaiting = carsTotal;
+	console.log("Spaces left: " + spaces);
+	// console.log("Cars Waiting: " + carsWaiting);
+	var carParked = "";
+	var carsParked = 0;
+	console.log("Cars Parked: " + carsParked);
+	while(spaces != 0){
+		for(spaces = 10; spaces != 0; spaces--){
+			makeName();
+			plateID();
+			hexID();
+
+			carsWaiting--;
+
+			carParked = "Car Parked: " + make + "; Plate ID: " + plate + "; Color: #" + hex + ";";
+			console.log(carParked);
+
+			console.log("Cars Waiting: " + carsWaiting);
+
+			carsParked++;
+
+		}
+
+			// var possibleSec = '0123456789';
+			timerTime = Math.floor(Math.random() * 5)
+		var timerId;
+		timerId = setInterval(timerTime, alert("hello"));
+		// timer();
+		console.log(timerId);
+
+
+		// console.log("Spaces left: " + spaces);
+		// console.log("Cars Parked: " + carsParked);
+	}
+}
+
+function carLeaves(){
+
+}
+
+
+
+
+
+			// proxy.addEventListener("dogBarked", dogBarked);
+
+
+
+
+
+
+
+		// function noMoreHonking() {
+		// 	clearInterval(timerId);
+		// }
+		// function carLeaves() {
+		// 	spaces++;
+		// }
+
+		// startHonking();
