d066bf33a0b09cd3f8d60faf89a263f76b698a6a
glz-diego <glzdiego12@gmail.com>
2019-04-10 19:50:08 -0500

    init message

--- script.js
+++ script.js
@@ -2,38 +2,43 @@ var spaces = 10;
 var carsTotal = 100;
 var carsGone = 0;
 
-	var make = "";
-function makeName(){
-	var brand = ["Ford", "Mustang", "Nissan", "Jeep", "Toyota", "Honda", "Porsche", "Volvo", "Subaru", "Chevrolet"];
-	make = brand[Math.floor(Math.random() * 10)];
+function CarInfo() {
+	this.make = makeName();
+	this.plate = plateID();
+	this.hex = hexID();
+	this.timerId = timer();
 }
 
-	var plate = "";
+
+function makeName(){
+	var brand = ["Ford", "Mustang", "Nissan", "Jeep", "Toyota", "Honda", "Porsche", "Volvo", "Subaru", "Chevrolet"];
+	return brand[Math.floor(Math.random() * 10)];
+}
+
 function plateID(){
-	plate = "";
+	var plate = "";
 	var id = 'qwertyuiopasdfghjklzxcvbnm0123456789';
 	for (var i = 0; i <= 6; i++) {
 		plate += id.charAt(Math.floor(Math.random() * 36))
 	}
+	return plate;
 }
 
-	var hex = "";
 function hexID(){
-	hex = "";
+	var hex = "";
 	var colorID = '0123456789';
 	for (var i = 0; i <= 5; i++) {
 		hex += colorID.charAt(Math.floor(Math.random() * 10))
 	}
+	return hex;
 }
 
-	var timerTime = "";
-// function timer() {
-// 	timerTime = "";
-// 	var possibleSec = '0123456789';
-// 	for (var i = 0; i <= 1; i++) {
-// 		timerTime += possibleSec.charAt(Math.floor(Math.random() * 5))
-// 	}
-// }
+function timer() {
+	var possibleSec = '012345';
+	var timerTime = possibleSec.charAt(Math.floor(Math.random() * 5000));
+	var timerId = timeOut(timerTime, );
+	return timerId;
+}
 
 function clearInterval() {
 	clearInterval(timerId);
@@ -54,8 +59,10 @@ function park(){
 
 			carsWaiting--;
 
-			carParked = "Car Parked: " + make + "; Plate ID: " + plate + "; Color: #" + hex + ";";
-			console.log(carParked);
+			// carParked = "Car Parked: " + make + "; Plate ID: " + plate + "; Color: #" + hex + ";";
+			// console.log(carParked);
+
+			console.log(carInfo());
 
 			console.log("Cars Waiting: " + carsWaiting);
 
@@ -63,40 +70,7 @@ function park(){
 
 		}
 
-			// var possibleSec = '0123456789';
-			timerTime = Math.floor(Math.random() * 5)
-		var timerId;
-		timerId = setInterval(timerTime, alert("hello"));
-		// timer();
-		console.log(timerId);
-
-
 		// console.log("Spaces left: " + spaces);
 		// console.log("Cars Parked: " + carsParked);
 	}
 }
-
-function carLeaves(){
-
-}
-
-
-
-
-
-			// proxy.addEventListener("dogBarked", dogBarked);
-
-
-
-
-
-
-
-		// function noMoreHonking() {
-		// 	clearInterval(timerId);
-		// }
-		// function carLeaves() {
-		// 	spaces++;
-		// }
-
-		// startHonking();
