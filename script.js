46eb1ac35f48c7ae0841f7711e59cd0b970eea13
glz-diego <glzdiego12@gmail.com>
2019-04-13 12:10:10 -0500

    init message

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
-var MAX_SPACES = 10;
-const carsTotal = 100;
-
 var carsWaiting = [];
 var parkingLot = [];
 var carsGone = [];
+
+const carsTotal = 20;
+var MAX_SPACES = 10;
+
+var spacesOpen = MAX_SPACES - parkingLot.length;
+
+// var interval = setInterval(function(){Valet()}, 1000);
+
 
 function carInfo() {
 	this.make = makeName();
 	this.plate = plateID();
 	this.hex = hexID();
-	// this.timerTime = timer();
+	this.timerTime = timer();
 
 	this.park = function(car){
-		console.log(car);
+		console.log("parking car", car);
 		parkingLot.push(car);
+		setTimeout(function(){ car.leave(car) }, car.timerTime);
+	}
+
+	this.leave = function(car){
+		console.log("leave", car);
+		// TODO: find cars position hint look up array.map and indexOf()
+		// parkingLot.pop(carPosition);
 	}
 }
 
 function carFactory(){
-	for (var i = 0; i < 100; i++) {
+	for (var i = 0; i < carsTotal; i++) {
 		var car = new carInfo();
 		carsWaiting.push(car);
 	}
@@ -49,14 +61,9 @@ function hexID(){
 	return hex;
 }
 
-// function timer() {
-// 	var possibleSec = '012345';
-// 	var timerNum = possibleSec.charAt(Math.floor(Math.random() * 5000));
-// 	var timerTime = timeOut(timerNum, function(){
-
-// 	});
-// 	return timerNum;
-// }
+function timer() {
+	return Math.random() * (5000 - 1000) + 1000;
+}
 
 function popCar(){
 	var car = carsWaiting.pop();
@@ -69,17 +76,18 @@ function popCar(){
 // }
 
 function Valet(){
-	// TODO: check if parkingLot length is less than max spaces 
-	if (parkingLot.length <= MAX_SPACES) {
-		for (var i = 0; i < MAX_SPACES; i++) {
+	console.log("Calling the Valet!")
+
+	if(carsWaiting.length !== 0){
+		console.log("cars are waiting!", spacesOpen)
+		for (var i = 0; i < spacesOpen; i++) {
 			var car = carsWaiting.pop();
 			car.park(car);
 		}
+	} else {
+		clearInterval(interval)
 	}
-	// TODO: loop until max spaces is hit
-	// TODO: pop a car off of the cars waiting array and set to a variable
-	// TODO: call park function passing car through 
-console.log(carsWaiting);
 }
+console.log("carsWaiting.length", carsWaiting.length);
 
 carFactory();
